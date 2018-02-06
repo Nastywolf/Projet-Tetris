1e49b73af4300bc31f7e9750697f0ff63a45c5f2
Bin <clement.bin@hotmail.fr>
2018-02-06 16:07:50 +0100

    faqcontroller

---
+++ tetris-Mvc/src/main/java/fr/formation/FAQController.java
@@ -0,0 +1,27 @@
+package fr.formation;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import fr.formation.tetris_dao.IFaqDAO;
+
+@Controller
+@RequestMapping(value = "/faq")
+public class FAQController {
+
+	@Autowired
+	private IFaqDAO daoProduit;
+
+
+
+
+}

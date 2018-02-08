cf2d3a15af609131dee8b9f51a25b701e1c5c559
Bin <clement.bin@hotmail.fr>
2018-02-08 11:31:13 +0100

    ça cours

--- tetris-Mvc/src/main/java/fr/formation/TetriminoController.java
+++ tetris-Mvc/src/main/java/fr/formation/TetriminoController.java
@@ -1,5 +1,6 @@
 package fr.formation;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +17,17 @@ import fr.formation.tetris_dao.ITetriminoDAO;
 import fr.formation.tetris_model_tetrimino.TetriminoModel;
 
 @Controller
-@RequestMapping(value="/tetrimino")
+@RequestMapping(value = "/tetrimino")
 public class TetriminoController {
 
 	@Autowired
 	private ITetriminoDAO daoTetrimino;
-	
-	
+
 	@ModelAttribute("page")
 	public String page() {
 		return "tetrimino";
 	}
-	
+
 	@GetMapping("")
 	public String liste(Model model) {
 		model.addAttribute("tetrimino", daoTetrimino.findAll());
@@ -39,14 +39,14 @@ public class TetriminoController {
 		model.addAttribute("mode", "ajouter");
 		return "tetriminotaille";
 	}
-	
+
 	@GetMapping("/tailleed")
-	public String taille(@RequestParam("id") int id, Model model){
+	public String taille(@RequestParam("id") int id, Model model) {
 		model.addAttribute("mode", "editer");
 		model.addAttribute("id", id);
 		return "tetriminotaille";
 	}
-	
+
 	@GetMapping("/ajouter")
 	public String ajouter(Model model, @RequestParam int taille) {
 		model.addAttribute("taille", taille);
@@ -55,38 +55,73 @@ public class TetriminoController {
 	}
 
 	@PostMapping("/ajouter")
-	public String ajouter(@Valid @ModelAttribute("tetrimino") TetriminoModel tetrimino,BindingResult result, Model model) {
-		if(result.hasErrors()) {
+	public String ajouter(@Valid @ModelAttribute("tetrimino") TetriminoModel tetrimino, BindingResult result,
+			Model model, HttpServletRequest request) {
+		if (result.hasErrors()) {
 			return "tetriminoedit";
 		}
+
+		int taille = Integer.parseInt(request.getParameter("taille"));
+		String val = "";
+		String res = "";
+		for (int i = 1; i < taille+1; i++) {
+			for (int j = 1; j < taille+1; j++) {
+				val = request.getParameter(i + "." + j);
+				if (val != null) {
+					res += "1";
+				} else {
+					res += "0";
+				}
+				res += ",";
+			}
+			res += "/";
+		}
+		tetrimino.setForme(res);
 		daoTetrimino.save(tetrimino);
-		
+
 		return "redirect:.";
 	}
-	
+
 	@GetMapping("/editer")
-	public String editer( @RequestParam("id") int id, Model model, @RequestParam("taille") int taille) {
+	public String editer(@RequestParam("id") int id, Model model, @RequestParam("taille") int taille) {
 		model.addAttribute("taille", taille);
 		model.addAttribute("tetrimino", daoTetrimino.findById(id).get());
 		return "tetriminoedit";
 	}
-	
+
 	@PostMapping("/editer")
-	public String editer( @Valid @ModelAttribute("tetrimino") TetriminoModel tetrimino, BindingResult result, @RequestParam("id") int id, Model model) {
-		if(result.hasErrors()) {
+	public String editer(@Valid @ModelAttribute("tetrimino") TetriminoModel tetrimino, BindingResult result,
+			@RequestParam("id") int id, Model model, HttpServletRequest request) {
+		if (result.hasErrors()) {
 			System.out.println(result.getAllErrors());
 			return "tetriminoedit";
 		}
 
+		int taille = Integer.parseInt(request.getParameter("taille"));
+		String val = "";
+		String res = "";
+		for (int i = 1; i < taille+1; i++) {
+			for (int j = 1; j < taille+1; j++) {
+				val = request.getParameter(i + "." + j);
+				if (val != null) {
+					res += "1";
+				} else {
+					res += "0";
+				}
+				res += ",";
+			}
+			res += "/";
+		}
+		tetrimino.setForme(res);
 		daoTetrimino.save(tetrimino);
-		
+
 		return "redirect:.";
 	}
-	
+
 	@GetMapping("/supprimer")
 	public String supprimer(@RequestParam("id") int id) {
 		daoTetrimino.deleteById(id);
-		
+
 		return "redirect:.";
 	}
 
